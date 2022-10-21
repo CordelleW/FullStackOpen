fc4fc4b3fceaee220bea96b5a891f8e14e9b42e5
Cordelle Walden <cordelle7@gmail.com>
2022-10-21 18:16:37 -0400

    destructuring props

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,17 @@
 const Hello = (props) => {
+  const name = props.name
+  const age = props.age
+
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
+
 
 const App = () => {
   const name = 'Peter'
